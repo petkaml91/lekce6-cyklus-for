3e5a219b33b9848e8556d9a627c2558623a01d8c
petkaml91 <petkaml91@gmail.com>
2021-04-17 23:44:37 +0100

    ukol while

--- main.js
+++ main.js
@@ -7,7 +7,15 @@ console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
+//for (let i=0; i<=10; i=i+1) {
+//   console.log(i);
+//}
+
+let a = 0;
+while (a <= 10) {
+    console.log(a);
+    a++;
+}
 
 
 
@@ -19,13 +27,27 @@ console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
+//for (let i=0; i<10; i=i+1) {
+//    console.log(i);
+//}
 
-
+let b=0;
+while (b < 10) {
+    console.log(b);
+    b++;
+}
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
+//for (let i=10; i>=0; i=i-1) {
+//    console.log(i);
+//}
+
+let c = 10;
+while (c >= 0) {
+    console.log(c);
+    c--;
+}
